bbeee3376099ab385a8ba111fc07e6f149a6626b
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-09-24 23:10:23 +0200

    TODO : time()

--- include/mcmp/module/secu_bypass.php
+++ include/mcmp/module/secu_bypass.php
@@ -2,6 +2,7 @@
 if(isset($_SESSION['nom'])){
     if (isset($_COOKIE['ticket']) AND $_COOKIE['ticket'] == $_SESSION['ticket'])
     {
+        // Controle Ok !
         $ticket = session_id().microtime().rand(1,999);
         $ticket = hash('sha512', $ticket);
         $_SESSION['ticket'] = $ticket;
@@ -10,7 +11,6 @@ if(isset($_SESSION['nom'])){
     else
     {
         // On détruit la session
-        //$_SESSION = array();
         session_destroy();
         unset($_COOKIE['ticket']);
         header('location:index.php');
